ab794b87de150ff4a7e404a91f5ba47abd89d599
salomao <salomao@gmail>
2026-06-03 08:41:21 -0300

    banco atualizado

--- frontend/banco.sql
+++ frontend/banco.sql
@@ -1,17 +1,19 @@
-create database if not exists gestao_academica;
-use	gestao_academica;
+CREATE DATABASE IF NOT EXISTS gestao_academica;
+USE gestao_academica;
+
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     ra VARCHAR(20) UNIQUE,
+    cpf VARCHAR(14) UNIQUE,
     email VARCHAR(150) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
     tipo_usuario ENUM('aluno', 'professor', 'admin') NOT NULL DEFAULT 'aluno',
-    matricula VARCHAR(20) UNIQUE,
     telefone VARCHAR(20),
     data_nascimento DATE,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE boletim (
     id INT PRIMARY KEY AUTO_INCREMENT,
