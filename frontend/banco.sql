1a00d037e3e625bc437bd8eefd2eb710f28e81a6
salomao <salomao@gmail>
2026-06-12 15:02:16 -0300

    Banco de dados com novas tabelas

--- frontend/banco.sql
+++ frontend/banco.sql
@@ -26,3 +26,40 @@ CREATE TABLE boletim (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
     FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
 );
+
+CREATE TABLE planejamento_aulas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data_aula DATE NOT NULL,
+    horario_inicio TIME NOT NULL,
+    horario_fim TIME NOT NULL,
+    serie VARCHAR(20) NOT NULL,
+    status ENUM('Planejada', 'Realizada', 'Cancelada') DEFAULT 'Planejada',
+    titulo VARCHAR(200) NOT NULL,
+    objetivos TEXT ,
+    metodologia TEXT ,
+    recursos TEXT 
+);
+
+CREATE TABLE comunicados (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    professor_id INT NOT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    mensagem VARCHAR(200) NOT NULL,
+    data_publicacao DATE NOT NULL,
+    turma VARCHAR(100) NOT NULL,
+    tipo ENUM('importante', 'informacao', 'evento') NOT NULL,
+    FOREIGN KEY (professor_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE agenda_escolar (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    professor_id INT NOT NULL,
+    titulo VARCHAR(50) NOT NULL,
+    descricao VARCHAR(100),
+    turma VARCHAR(100) NOT NULL,
+    data_evento DATE NOT NULL,
+    horario_inicio TIME,
+    horario_fim TIME,
+    tipo ENUM('Prova', 'Trabalho', 'Reuniao', 'Evento') NOT NULL,
+    FOREIGN KEY (professor_id) REFERENCES usuarios(id)
+);  
